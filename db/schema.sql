12c9efece8fceca088c805f3904281a4727cda67
BasedAtlas <tsaiwilliam888@gmail.com>
2023-11-10 16:52:26 -0800

    Minor update to SQL

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,21 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
-USE employees_db;
+-- Insert data into the department table
+INSERT INTO department (id, name) VALUES
+    (1, 'Sales'),
+    (2, 'Marketing'),
+    (3, 'Engineering');
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR (30) NOT NULL 
-);
+-- Insert data into the role table
+INSERT INTO role (id, title, salary, department_id) VALUES
+    (1, 'Sales Manager', 70000, 1),
+    (2, 'Sales Representative', 50000, 1),
+    (3, 'Marketing Coordinator', 45000, 2),
+    (4, 'Software Engineer', 80000, 3),
+    (5, 'QA Engineer', 70000, 3);
 
-CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR (30) NOT NULL, 
-    salary DECIMAL(10,2),
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL 
-);
-
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR (30),
-    last_name VARCHAR (30),
-    role_id INT,
-    manager_id INT,
-);
+-- Insert data into the employee table
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
+    (1, 'John', 'Doe', 1, NULL),
+    (2, 'Jane', 'Smith', 2, 1),
+    (3, 'Bob', 'Johnson', 3, NULL),
+    (4, 'Alice', 'Williams', 4, NULL),
+    (5, 'Charlie', 'Brown', 5, 4);
